620abe9e67263048d468ea076c98f3c8e5576228
Siegel <2285448337@qq.com>
2024-08-23 19:27:36 +0800

    项目初始化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,19 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole" />
+    <div>
+      <h2>dashboard</h2>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
 
 export default {
   name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
   data() {
-    return {
-      currentRole: 'adminDashboard'
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'roles'
-    ])
+    return {}
   },
   created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
-    }
   }
 }
 </script>
