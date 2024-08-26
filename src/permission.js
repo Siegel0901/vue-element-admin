3e18bad2cf7250082df07bcaea666acae4133646
Siegel <2285448337@qq.com>
2024-08-26 11:56:20 +0800

    项目配置

--- src/permission.js
+++ src/permission.js
@@ -1,21 +1,20 @@
 import router from './router'
 import store from './store'
-import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
+import defaultSettings from './settings'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+const whiteList = ['/login'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
   // start progress bar
   NProgress.start()
 
   // set page title
-  document.title = getPageTitle(to.meta.title)
+  document.title = `${to.meta.title ? to.meta.title + ' - ' : ''}${defaultSettings.title}`
 
   // determine whether the user has logged in
   const hasToken = getToken()
@@ -24,7 +23,7 @@ router.beforeEach(async(to, from, next) => {
     if (to.path === '/login') {
       // if is logged in, redirect to the home page
       next({ path: '/' })
-      NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
+      NProgress.done()
     } else {
       // determine whether the user has obtained his permission roles through getInfo
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
@@ -35,7 +34,6 @@ router.beforeEach(async(to, from, next) => {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
           const { roles } = await store.dispatch('user/getInfo')
-
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
 
@@ -48,7 +46,7 @@ router.beforeEach(async(to, from, next) => {
         } catch (error) {
           // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
+          console.log(error)
           next(`/login?redirect=${to.path}`)
           NProgress.done()
         }
--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,29 @@ const name = defaultSettings.title || 'vue Element Admin' // page title
 // For example, Mac: sudo npm run
 // You can change the port by the following method:
 // port = 9527 npm run dev OR npm run dev --port = 9527
-const port = process.env.port || process.env.npm_config_port || 9527 // dev port
+const port = process.env.port || process.env.npm_config_port || 8080 // dev port
+
+const gatewayUrl = 'http://localhost:8002/api'
+const devUrlMap = {
+  'user-api': 'http://localhost:8080'
+}
+const prodUrlMap = {
+  'user-api': gatewayUrl + '/user'
+}
+const devMode = process.env.NODE_ENV !== 'production'
+// const debugMode = true;
+const urlMap = devMode ? devUrlMap : prodUrlMap
+const proxyTable = {}
+for (const key in urlMap) {
+  proxyTable['/' + key] = {
+    target: urlMap[key],
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+      ['^/' + key]: '/'
+    }
+  }
+}
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -36,7 +58,7 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    proxy: proxyTable
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
@@ -88,7 +110,7 @@ module.exports = {
             .plugin('ScriptExtHtmlWebpackPlugin')
             .after('html')
             .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
+              // `runtime` must same as runtimeChunk name. default is `runtime`
               inline: /runtime\..*\.js$/
             }])
             .end()
